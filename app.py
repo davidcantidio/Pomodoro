f907cfe95efddcc5d1b4ac13546ac8687d56f268
David Cantidio <dbcantidio@gmail.com>
2022-11-17 17:38:11 -0300

    Added icon to app and setup dimensions of the window menu

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from tkinter import *
+from config import *
 
 menu = Tk()
 menu.title("Pomodoro")
-menu.geometry("300x200+200+200")
+menu.geometry(f'{screen_max_width}x{screen_max_height}+{screen_pos_x}+{screen_pos_y}')
 menu.resizable(True, True)
-menu.minsize()
+menu.minsize(int(screen_min_width), int(screen_min_height))
+menu.maxsize(int(screen_max_width), int(screen_max_height))
+menu.iconbitmap("icon/pomodoro.ico")
 menu.mainloop()
--- config.py
+++ config.py
@@ -1,1 +1,6 @@
-screen_size=""
+screen_max_width = "500"
+screen_max_height = "250"
+screen_pos_x="200"
+screen_pos_y="200"
+screen_min_width = "300"
+screen_min_height= "150"
