efc6b0f1f21a6c21187956618e98efa7f3750b13
David Cantidio <dbcantidio@gmail.com>
2022-11-17 18:49:49 -0300

    Start, pause and reset buttons created

--- app.py
+++ app.py
@@ -7,5 +7,19 @@ menu.geometry(f'{screen_max_width}x{screen_max_height}+{screen_pos_x}+{screen_po
 menu.resizable(True, True)
 menu.minsize(int(screen_min_width), int(screen_min_height))
 menu.maxsize(int(screen_max_width), int(screen_max_height))
-menu.iconbitmap("icon/pomodoro.ico")
+
+#Icon setup
+icon = PhotoImage(file="icon/pomodoro.png")
+menu.tk.call('wm', 'iconphoto', menu._w, icon)
+menu.iconphoto(False, icon)
+
+
+#Start Button
+btn_start = Button(menu, text = "start")
+btn_reset = Button(menu, text = "reset")
+btn_pause = Button(menu, text= "pause")
+btn_start.pack()
+btn_reset.pack()
+btn_pause.pack()
+
 menu.mainloop()
--- config.py
+++ config.py
@@ -4,3 +4,4 @@ screen_pos_x="200"
 screen_pos_y="200"
 screen_min_width = "300"
 screen_min_height= "150"
+ 
