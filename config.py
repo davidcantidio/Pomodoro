4638c9c34ce0f205a64f3f5cc90a2471e85801e2
David Cantidio <dbcantidio@gmail.com>
2022-11-20 14:12:10 -0300

    Solved button icons not showing

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-screen_max_width = "600"
-screen_max_height = "400"
-screen_pos_x="200"
-screen_pos_y="200"
-screen_min_width = "300"
-screen_min_height= "150"
+screen_max_width = 600
+screen_max_height = 400
+screen_pos_x=200
+screen_pos_y=200
+screen_min_width = 300
+screen_min_height= 150
  
