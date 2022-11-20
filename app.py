c7d2a094b9b076f785802a3ed60007f6663f13ec
David Cantidio <dbcantidio@gmail.com>
2022-11-20 14:12:30 -0300

    Solved button icons not showing

--- app.py
+++ app.py
@@ -1,132 +1,126 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk
 from config import *
 from tkfontawesome import icon_to_image
-import time
 
-root = Tk()
-root.title("Pomodoro")
-root.geometry(f'{screen_max_width}x{screen_max_height}+{screen_pos_x}+{screen_pos_y}')
-root.resizable(True, True)
-root.minsize(int(screen_min_width), int(screen_min_height))
-root.maxsize(int(screen_max_width), int(screen_max_height))
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Pomodoro")
+        self.geometry(f'{screen_max_width}x{screen_max_height}+{screen_pos_x}+{screen_pos_y}')
+        self.resizable(True, True)
+        self.minsize(screen_min_width, screen_min_height)
+        self.maxsize(screen_max_width, screen_max_height)
+        # #Icon setup
+        icon = icon_to_image("clock", fill="#3a3b3c", scale_to_width=45)
+        self.iconphoto(False, icon)
+        
+        #creating a container 
+        settings_frame = tk.Frame(self, bg='pink')
+        settings_frame.place(x=10, y=10)
+        periods_frame = tk.Frame(settings_frame, bg='yellow')
+        periods_frame.pack(side = 'left')
 
-#Icon setup
-icon = PhotoImage(file="icon/pomodoro.png")
-root.tk.call('wm', 'iconphoto', root._w, icon)
-root.iconphoto(False, icon)
+        self.chsn_wrk_mins = tk.StringVar()
+        self.work_mins_entry = tk.Scale(periods_frame, variable=self.chsn_wrk_mins, from_=work_scale_min, to=work_scale_max, resolution=5, orient='horizontal', label='Trabalho (min)', length=270)
+        self.work_mins_entry.pack(padx=5, pady=2, side='top', fill='x')
 
-#Settings frame
-settings_frame = Frame(root, bg="pink")
-settings_frame.place(x=10, y=10)
+        self.sm_break_mins_entry = tk.StringVar()
+        self.sm_break_mins_entry = tk.Scale(periods_frame, from_=sm_break_min, to=sm_break_max, resolution=5, orient='horizontal', label='Pausa menor', length=270)
+        self.sm_break_mins_entry.pack(padx=5, pady=2, side='top', fill='x')
+        
+        self.bg_break_mins_entry = tk.StringVar()
+        self.bg_break_mins_entry = tk.Scale(periods_frame, from_=bg_break_min, to=bg_break_max, resolution=5, orient='horizontal', label='Pausa maior', length=270)
+        self.bg_break_mins_entry.pack(padx=5, pady=2, side='top', fill='x')
 
-    #Frame with horizontal bars to choose periods os intervals
-periods_frame = Frame(settings_frame, bg="yellow")
-periods_frame.pack(side = LEFT)
-cicles_frame = Frame(settings_frame, bg="red")
-cicles_frame.pack(side = RIGHT, fill=Y)
+        cicles_frame = tk.Frame(settings_frame, bg='red')
+        cicles_frame.pack(side = 'right', fill='y')
+        self.cicles_n_entry = tk.StringVar()
+        self.bg_break_mins_entry = tk.Scale(cicles_frame, from_=cicles_min, to=cicles_max, resolution=1, orient='vertical', label='Ciclos', length=190)
+        self.bg_break_mins_entry.pack(padx=5, pady=2, side='top', fill='x')
 
-        # Use of Scale class so the user can select amount of minutes for each step of pomodoro
-work_minsEntry= Scale(periods_frame, from_=30, to=90, resolution=5, orient=HORIZONTAL, label="Trabalho (min)", length=270)
-work_minsEntry.pack(padx=5, pady=2, side=TOP, fill='x')
+        controls_frame = tk.Frame(self, bg="blue")
+        controls_frame.place(x=500, y=10, height=100)
+            #Buttons
+                #Play/Pause button
+        self.btn_playpause_icon = icon_to_image("play", fill="#3a3b3c", scale_to_width=15)
+        
+        self.btn_playpause= tk.Button(controls_frame, image = self.btn_playpause_icon, width=30, height=30, command=print("timer"))
+        self.btn_playpause.pack(padx=5, pady=2)  
 
-small_break_minsEntry= Scale(periods_frame, from_=5, to=30, resolution=5, orient=HORIZONTAL, label="Pausa menor (min)", length=150)
-small_break_minsEntry.pack(padx=5, pady=2, side=TOP, fill='x')
-  
-big_break_minsEntry= Scale(periods_frame, from_=10, to=60, resolution=5, orient=HORIZONTAL, label="Pausa maior (min)", length=180)
-big_break_minsEntry.pack(padx=5, pady=2, side=TOP, fill='x')
-  
-cicleEntry = Scale(cicles_frame, from_=2, to=6, orient=VERTICAL, label="Ciclos até pausa maior")
-cicleEntry.pack(padx=5, pady=2, side=RIGHT, fill=Y)
+                #Reset Button
+        self.btn_reset_icon = icon_to_image("history", fill="#3a3b3c", scale_to_width=15)
+        self.btn_reset = tk.Button(controls_frame, image = self.btn_reset_icon, width=30, height=30)
+        self.btn_reset.pack(padx=5, pady=2)
 
-#Control frame
-controls_frame = Frame(root, bg="blue")
-controls_frame.place(x=500, y=10, height=100)
-    #Buttons
-        #Play/Pause button
-btn_playpause_icon = icon_to_image("play", fill="#3a3b3c", scale_to_width=15)
-btn_playpause= Button(controls_frame, image = btn_playpause_icon, width=30, height=30, command=print("play"))
-btn_playpause.pack(padx=5, pady=2)
+        display_frame = tk.Frame(self, bg="red")
+        display_frame.place(x=10, y=450)
 
+        self.chsn_wrk_mins.set(f'{str(self.chsn_wrk_mins)} mins.')
+        self.wrk_mins_label = tk.Label(display_frame, textvariable=self.text)
+        self.wrk_mins_label.pack(side='right')
 
-        #Reset Button
-btn_reset_icon = icon_to_image("history", fill="#3a3b3c", scale_to_width=15)
-btn_reset = Button(controls_frame, image = btn_reset_icon, width=30, height=30)
-btn_reset.pack(padx=5, pady=2)
-#btn_pause_icon = icon_to_image("pause", fill="#3a3b3c", scale_to_width=15)
-
-
-
-
-
-
-
-
-
-#Display frame
-display_frame = Frame(root, bg="red")
-display_frame.place(x=10, y=250)
+                # Declaration of variables
+        
 
 
-    #progress bars
-        #Work progress bar
-pb_work = ttk.Progressbar(display_frame, orient='horizontal', mode='indeterminate')
-pb_work.pack(side = LEFT)
-        # Small break progress bar
-pb_sm_break = ttk.Progressbar(display_frame, orient='horizontal', mode='indeterminate')
-pb_sm_break.pack(side = LEFT)
 
-        # Small breakIntervalo progress bar
-pb_big_break = ttk.Progressbar(display_frame, orient='horizontal', mode='indeterminate')
-pb_big_break.pack(side = LEFT)
-def submit():
-    try:
-        # the input provided by the user is
-        # stored in here :temp
-        temp = int(hour.get())*3600 + int(minute.get())*60 + int(second.get())
-    except:
-        print("Please input the right value")
-    while temp >-1:
+    def currentTask(self):
+        pass
+
+    def startTimer(self):
+        pass
+
+# def submit():
+#     try:
+#         # the input provided by the user is
+#         # stored in here :temp
+#         temp = int(hour.get())*3600 + int(minute.get())*60 + int(second.get())
+#     except:
+#         print("Please input the right value")
+#     while temp >-1:
          
-        # divmod(firstvalue = temp//60, secondvalue = temp%60)
-        mins,secs = divmod(temp,60)
+#         # divmod(firstvalue = temp//60, secondvalue = temp%60)
+#         mins,secs = divmod(temp,60)
   
-        # Converting the input entered in mins or secs to hours,
-        # mins ,secs(input = 110 min --> 120*60 = 6600 => 1hr :
-        # 50min: 0sec)
-        hours=0
-        if mins >60:
+#         # Converting the input entered in mins or secs to hours,
+#         # mins ,secs(input = 110 min --> 120*60 = 6600 => 1hr :
+#         # 50min: 0sec)
+#         hours=0
+#         if mins >60:
              
-            # divmod(firstvalue = temp//60, secondvalue
-            # = temp%60)
-            hours, mins = divmod(mins, 60)
+#             # divmod(firstvalue = temp//60, secondvalue
+#             # = temp%60)
+#             hours, mins = divmod(mins, 60)
          
-        # using format () method to store the value up to
-        # two decimal places
-        hour.set("{0:2d}".format(hours))
-        minute.set("{0:2d}".format(mins))
-        second.set("{0:2d}".format(secs))
+#         # using format () method to store the value up to
+#         # two decimal places
+#         hour.set("{0:2d}".format(hours))
+#         minute.set("{0:2d}".format(mins))
+#         second.set("{0:2d}".format(secs))
   
-        # updating the GUI window after decrementing the
-        # temp value every time
-        root.update()
-        time.sleep(1)
+#         # updating the GUI window after decrementing the
+#         # temp value every time
+#         root.update()
+#         time.sleep(1)
   
-        # when temp value = 0; then a messagebox pop's up
-        # with a message:"Time's up"
-        if (temp == 0):
-            messagebox.showinfo("Time Countdown", "Time's up ")
+#         # when temp value = 0; then a messagebox pop's up
+#         # with a message:"Time's up"
+#         if (temp == 0):
+#             messagebox.showinfo("Time Countdown", "Time's up ")
          
-        # after every one sec the value of temp will be decremented
-        # by one
-        temp -= 1
+#         # after every one sec the value of temp will be decremented
+#         # by one
+#         temp -= 1
+ 
+# # button widget
+# btn = Button(root, text='Set Time Countdown', bd='5',
+#              command= submit)
+# btn.place(x = 70,y = 120)
+  
+# # infinite loop which is required to
+# # run tkinter program infinitely
+# # until an interrupt occurs
 
-def change_btn ():
-    if btn_start.image == btn_pause_icon:
-        #start timer
-
-        btn_start.config(image=btn_pause_icon)
-        btn_start.image = btn_pause_icon
-
-
-root.mainloop()
+if __name__== "__main__":
+    app = App()
+    app.mainloop()
--- config.py
+++ config.py
@@ -4,4 +4,13 @@ screen_pos_x=200
 screen_pos_y=200
 screen_min_width = 300
 screen_min_height= 150
- 
+
+#Meta settings
+work_scale_min = 30 
+work_scale_max = 90
+sm_break_min = 5 
+sm_break_max = 15
+bg_break_min = 15
+bg_break_max = 45
+cicles_min = 2 
+cicles_max = 6
